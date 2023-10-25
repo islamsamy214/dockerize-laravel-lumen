29cbc35a92fb9ed9f0d7957a9a5bf4340c27dbb6
islam samy <64733740+islamsamy214@users.noreply.github.com>
2023-10-25 11:50:41 +0400

    Create start-container.sh

---
+++ start-container.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+if [ ! -z "$WWWUSER" ]; then
+    usermod -u $WWWUSER sail
+fi
+
+if [ ! -d /.composer ]; then
+    mkdir /.composer
+fi
+
+chmod -R ugo+rw /.composer
+composer install --ignore-platform-reqs --no-interaction --no-progress --no-suggest --working-dir=/var/www/html
+
+if [ $# -gt 0 ]; then
+    exec gosu $WWWUSER "$@"
+else
+    exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
+fi
